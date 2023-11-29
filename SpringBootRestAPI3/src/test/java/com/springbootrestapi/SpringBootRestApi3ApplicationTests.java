4258bf3f245aea28285873f338caf51bd57ac19f
altaf1232 <139534724+altaf1232@users.noreply.github.com>
2023-11-30 00:14:25 +0530

    Add files via upload

---
+++ SpringBootRestAPI3/src/test/java/com/springbootrestapi/SpringBootRestApi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApi3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
